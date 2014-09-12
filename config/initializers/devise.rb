981726ca2ea909b9e0f503820e8530f375555337
Rich Shea <riccor75@aol.com>
2014-09-11 21:34:49 -0600

    fixed username and pass

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,6 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = 'c6cfa847cf1d707e2879736f29cc189125fb3c2f820e9e2e8616603983166fef34a612801ba4468351d11051c9911bea6514380661b61118e596caa897ca4a31'
 
   config.secret_key = ENV['DEVISE_SECRET_KEY']
 
